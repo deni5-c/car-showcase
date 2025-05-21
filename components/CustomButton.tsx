4c0ee6c3fdf749a97cc9565d8f517076537e57ea
deni5-c <denisbobeica18@gmail.com>
2025-05-21 22:00:43 +0300

    Added: Hero, Navbar, Footer, Custom Button Component [20%-Done]

---
+++ components/CustomButton.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { CustomButtonProps } from "@/types"
+import Image from "next/image"
+
+const CustomButton = ({ title, btnType, containerStyles, handleClick }: CustomButtonProps) => {
+  return (
+    <button
+        disabled={false}
+        type={btnType || "button"}
+        className={`flex flex-row relative justify-center items-center py-3 px-6 outline-none ${containerStyles}`}
+        onClick={handleClick}
+    >
+        <span className={`flex-1`}>{title}</span>
+    </button>
+  )
+}
+
+export default CustomButton
