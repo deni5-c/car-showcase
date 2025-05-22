c6de4ed65bd4b9ca2201b619cf00593d32ed720f
deni5-c <denisbobeica18@gmail.com>
2025-05-22 21:23:35 +0300

    bug-fixed - Added fixed some errors - Added style - Made website more responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,57 @@
-import Image from "next/image";
-
-import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
-import { fetchCarData } from "@/utils";
+import React from "react";
+import { fetchCars } from "@/utils";
 import { HomeProps } from "@/types";
 import { fuels, yearsOfProduction } from "@/constants";
+import { CarCard, SearchBar, CustomFilter, Hero } from "@/components";
 
+export default async function Home(props: HomeProps) {
+  const searchParams = await props.searchParams;
+  // const { manufacturer } = await searchParams
 
-export default async function Home({ searchParams }: HomeProps) {
-  const allCars = await fetchCarData({
+  const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
     fuel: searchParams.fuel || "",
-    limit: searchParams.limit || 10,
     model: searchParams.model || "",
   });
 
-
-
+  console.log(allCars);
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
-    <main className="overflow-hidden">
+    <main className='overflow-hidden'>
       <Hero />
-      <div className="max-w-[1440px] mx-auto mt-12 sm:px-16 px-6 py-4" id="discover">
-        <div className="flex flex-col items-start justify-start gap-y-2.5 text-black-100">
-          <h4 className="text-4xl font-bold">Car Catalogue</h4>
-          <p className="text-base font-normal">
-            Explore the cars you might like
-          </p>
+
+      <div className='max-w-[1440px] mx-auto mt-12 sm:px-16 px-6 py-4' id='discover'>
+        <div className='flex flex-col items-start justify-start gap-y-2.5 text-black-100'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
 
-        <div className="mt-12 w-full flex justify-between items-center flex-wrap gap-5">
+        <div className='mt-12 w-full flex justify-between items-center flex-wrap gap-5'>
           <SearchBar />
-          <div className="flex flex-row items-start justify-start gap-y-2.5 gap-x-2.5 text-black-100 z-0">
-            <CustomFilter title="fuel" options={fuels} />
-            <CustomFilter title="year" options={yearsOfProduction} />
+
+          <div className='flex justify-start flex-wrap items-center gap-2'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
 
-        { !isDataEmpty ? (
+        {!isDataEmpty ? (
           <section>
-            <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-14">
-              { allCars?.map((car) => <CarCard car={car}/> )}
+            <div className='grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-14'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
             </div>
           </section>
-        ): (
-          <div className="mt-16 flex justify-center items-center flex-col gap-2">
-            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+        ) : (
+          <div className='mt-16 flex justify-center items-center flex-col gap-2'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
-
       </div>
     </main>
   );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import React, {useState} from 'react'
-import { useRouter } from 'next/navigation'
-import Image from 'next/image'
-import SearchManufacturer from './SearchManufacturer'
-import { Button } from '@headlessui/react'
+import Image from "next/image";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import SearchManufacturer from "./SearchManufacturer";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
@@ -19,76 +19,75 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState('')
-  const [model, setModel] = useState('')
-  const router = useRouter()
-  
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  const router = useRouter();
+
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if(manufacturer === '' || model === '') {
-      return alert('Please fill in the search bar')
+    if (manufacturer.trim() === "" && model.trim() === "") {
+      return alert("Please provide some input");
     }
 
-    updateSearchParams(
-      model.toLowerCase(), 
-      manufacturer.toLowerCase()
-    )
-
-  }
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
 
   const updateSearchParams = (model: string, manufacturer: string) => {
-    const searchParams = new URLSearchParams(window.location.search)
+    // Create a new URLSearchParams object using the current URL search parameters
+    const searchParams = new URLSearchParams(window.location.search);
 
-    if(model) {
-      searchParams.set('model', model)
+    // Update or delete the 'model' search parameter based on the 'model' value
+    if (model) {
+      searchParams.set("model", model);
     } else {
-      searchParams.delete('model', model)
+      searchParams.delete("model");
     }
 
-    if(model) {
-      searchParams.set('manufacturer', manufacturer)
+    // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
     } else {
-      searchParams.delete('manufacturer', manufacturer)
+       searchParams.delete("manufacturer");
     }
-  
-    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
 
-    router.push(newPathname)
-  }
+    // Generate the new pathname with the updated search parameters
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
 
   return (
     <form className='flex items-center justify-start max-sm:flex-col w-full relative max-sm:gap-4 max-w-3xl' onSubmit={handleSearch}>
-        <div className='flex-1 max-sm:w-full flex justify-start items-center relative'>
-            <SearchManufacturer 
-                manufacturer={manufacturer}
-                setManufacturer={setManufacturer}    
-            />
-            <SearchButton otherClasses='sm:hidden' />
-        </div>
-
-        <div className='flex-1 max-sm:w-full flex justify-start items-center relative'>
-          <Image
-            src="/model-icon.png"
-            alt='car model'
-            width={25}
-            height={25}
-            className='absolute w-[20px] h-[20px] ml-4'
-          />
-
-          <input 
-            type="text" 
-            name='model'
-            value={model}
-            onChange={(e) => setModel(e.target.value)}
-            placeholder='Tiguan'
-            className='w-full h-[48px] pl-12 p-4 bg-light-white rounded-r-full max-sm:rounded-full outline-none cursor-pointer text-sm'
-          />
-          <SearchButton otherClasses='sm:hidden' />
-        </div>
-        <SearchButton otherClasses='max-sm:hidden' />
+      <div className='flex-1 max-sm:w-full flex justify-start items-center relative'>
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManuFacturer}
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <div className='flex-1 max-sm:w-full flex justify-start items-center relative'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='w-full h-[48px] pl-12 p-4 bg-light-white rounded-r-full max-sm:rounded-full outline-none cursor-pointer text-sm'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
